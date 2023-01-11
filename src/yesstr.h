1df8ee4c6e24f9047f6936ee0cce074e44d36c5f
@RandyMcMillan <randy.lee.mcmillan@gmail.com>
2023-01-10 20:50:30 -0500

    fbs/yesstr.fbs: src/yesstr.h: make stuff

--- src/yesstr.h
+++ src/yesstr.h
@@ -3,27 +3,27 @@
 #include "golpe.h"
 
 
-inline void verifyYesstrRequest(std::string_view msg) {
+inline void verifyYesstrrequest(std::string_view msg) {
     if (!msg.starts_with("Y")) throw herr("invalid yesstr magic char");
     msg = msg.substr(1);
     auto verifier = flatbuffers::Verifier(reinterpret_cast<const uint8_t*>(msg.data()), msg.size());
-    bool ok = verifier.VerifyBuffer<Yesstr::Request>(nullptr);
+    bool ok = verifier.VerifyBuffer<Yesstr::request>(nullptr);
     if (!ok) throw herr("yesstr request verification failed");
 }
 
-inline void verifyYesstrResponse(std::string_view msg) {
+inline void verifyYesstrresponse(std::string_view msg) {
     if (!msg.starts_with("Y")) throw herr("invalid yesstr magic char");
     msg = msg.substr(1);
     auto verifier = flatbuffers::Verifier(reinterpret_cast<const uint8_t*>(msg.data()), msg.size());
-    bool ok = verifier.VerifyBuffer<Yesstr::Response>(nullptr);
+    bool ok = verifier.VerifyBuffer<Yesstr::response>(nullptr);
     if (!ok) throw herr("yesstr response verification failed");
 }
 
 
-inline const Yesstr::Request *parseYesstrRequest(std::string_view msg) {
-    return flatbuffers::GetRoot<Yesstr::Request>(msg.substr(1).data());
+inline const Yesstr::request *parseYesstrrequest(std::string_view msg) {
+    return flatbuffers::GetRoot<Yesstr::request>(msg.substr(1).data());
 }
 
-inline const Yesstr::Response *parseYesstrResponse(std::string_view msg) {
-    return flatbuffers::GetRoot<Yesstr::Response>(msg.substr(1).data());
+inline const Yesstr::response *parseYesstrResponse(std::string_view msg) {
+    return flatbuffers::GetRoot<Yesstr::response>(msg.substr(1).data());
 }
